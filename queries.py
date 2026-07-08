e011c419c9f4a1c6863e91bcaeaea73d57287f1f
pınar özdoğan <pinarozdogan2323@gmail.com>
2026-07-08 23:16:29 +0300

    Refactor SQL queries for product and category listing
    Updated SQL queries to improve categorization and ordering.

--- queries.py
+++ queries.py
@@ -1,29 +1,15 @@
 # queries.py
-
-# Ürünleri ve kategorileri listeleyen ana sorgu
-GET_URUNLER = """
-    SELECT Urunler.urun_id, Urunler.urun_adi, Urunler.fiyat, Kategoriler.kategori_adi 
+GET_KATEGORILER = """
+    SELECT DISTINCT Kategoriler.kategori_adi 
     FROM Urunler 
-    JOIN Kategoriler ON Urunler.kategori_id = Kategoriler.kategori_id 
-    ORDER BY Urunler.urun_id
+    INNER JOIN Kategoriler ON Urunler.kategori_id = Kategoriler.kategori_id 
+    ORDER BY Kategoriler.kategori_adi
 """
 
-# Arama sorgusu (bunu fonksiyonla birleştireceğiz)
-SEARCH_URUNLER = """
-    SELECT Urunler.urun_id, Urunler.urun_adi, Urunler.fiyat, Kategoriler.kategori_adi 
+GET_URUNLER_BY_KATEGORI = """
+    SELECT Urunler.urun_adi, Urunler.fiyat 
     FROM Urunler 
-    JOIN Kategoriler ON Urunler.kategori_id = Kategoriler.kategori_id 
-    WHERE Urunler.urun_adi ILIKE %s
+    INNER JOIN Kategoriler ON Urunler.kategori_id = Kategoriler.kategori_id 
+    WHERE Kategoriler.kategori_adi = %s 
+    ORDER BY Urunler.urun_adi
 """
-
-# Ürün ekleme
-INSERT_URUN = "INSERT INTO Urunler (urun_adi, fiyat, kategori_id) VALUES (%s, %s, %s)"
-
-# Ürün güncelleme
-UPDATE_URUN = "UPDATE Urunler SET urun_adi=%s, fiyat=%s WHERE urun_id=%s"
-
-# Ürün silme
-DELETE_URUN = "DELETE FROM Urunler WHERE urun_id = %s"
-
-# Kategori listesi
-GET_KATEGORILER = "SELECT kategori_id, kategori_adi FROM Kategoriler"
